600dc47110d99ea49bbf9c6832e2d5ea524c4202
明明不在 <847679250@qq.com>
2024-06-10 17:33:00 +0800

    feat: css样式增加个模糊遮罩

---
+++ docs/cssSecrets/examples/blurMask.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    收到浓缩粉山东房价哦叫哦三等奖佛山店叫哦技术附件山东房价欧式机
+    <div class="mask">
+    </div>
+  </div>
+</template>
+<style scoped>
+.container {
+  display: flex;
+  position: relative;
+  width: 200px;
+}
+.mask {
+  position: absolute;
+  width: 100%;
+  height: 62px;
+  z-index: 100;
+  left: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0);
+  backdrop-filter: blur(3px);
+}
+</style>
