b0532f27ccb1a311aeeedf24e1071e3f9007e7ec
enterprise_k18316 <k18316@nbcb.com>
2024-07-09 14:18:55 +0800

    feat: 移动端H5吸顶

---
+++ docs/blog/demo/StickyContent.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="sticky-container">
+    <div class="content1">content</div>
+    <div>
+      <div class="xi-ding">吸顶栏</div>
+      <div class="content2">
+        <div>11111</div>
+        <div>22222</div>
+        <div>33333</div>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.sticky-container {
+  color: white;
+  height: 400px;
+  width: 300px;
+  overflow-y: auto;
+}
+
+.xi-ding {
+  position: sticky;
+  height: 60px;
+  background: rebeccapurple;
+  top: 0px;
+}
+
+.content1 {
+  height: 100px;
+  background: salmon;
+}
+
+.content2 {
+  height: 600px;
+  background: darkcyan;
+}
+</style>
